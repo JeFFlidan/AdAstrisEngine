353d3fe2cbbbdb2b29cfe473e14f9dfe230f07cc
Kirill Zaverukha <zaveruha678@gmail.com>
2022-11-14 23:57:47 +0200

    Implemented parsing shaders using SPIRV-Reflect

--- src/vk_shaders.h
+++ src/vk_shaders.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include <vector>
 
 #include <vulkan/vulkan.h>
@@ -15,11 +16,37 @@ namespace vkutil
 			void delete_shader_module();
 
 			VkShaderModule get_shader_module() const { return shaderModule; }
-	 
+			std::vector<uint32_t> get_code() const { return code; }
+			
 			void spv_reflect_test();
 		private:
 			VkDevice device;
 			VkShaderModule shaderModule;
 			std::vector<uint32_t> code;
 	};
+	
+	struct ShaderEffect
+	{
+		// Storage information to create a VkPipeline
+		//VkPipelineLayout buildLayout;
+		std::vector<VkDescriptorSetLayout> setLayouts;
+		std::vector<VkDescriptorSetLayoutCreateInfo> layoutsCreateInfos;
+
+		struct ShaderStage
+		{
+			Shader* shader;
+			VkShaderStageFlagBits stage;
+		};
+
+		std::vector<ShaderStage> stages;
+
+		void add_stage(Shader* shader, VkShaderStageFlagBits shaderStage)
+		{
+			stages.push_back({shader, shaderStage});
+		}
+
+		VkPipelineLayout get_pipeline_layout(VkDevice device);
+		void destroy_shader_modules();
+	};
+
 }
