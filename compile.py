a5c5f50372eb4ab0e67bb49534ce7fa47e958e04
Kirill Zaverukha <zaveruha678@gmail.com>
2023-01-30 02:42:20 +0200

    Improved compile script

--- compile.py
+++ compile.py
@@ -3,9 +3,22 @@ import subprocess
 
 
 def compile_script():
+    root_path = os.getcwd()
     build_path = os.getcwd() + "\\build"
-    subprocess.check_call('cmake -G "MinGW Makefiles" ' + build_path, shell=True)
-    subprocess.check_call('cmake --build ' + build_path, shell=True)
+    isDir = os.path.isdir(build_path)
+    needsReconfig = True
+    if not isDir:
+        os.mkdir(build_path)
+        subprocess.check_call('cmake -G "MinGW Makefiles" -B ' + build_path, shell=True)
+        needsReconfig = False
+
+    if needsReconfig:
+        subprocess.check_call('cmake -G "MinGW Makefiles" ' + build_path, shell=True)
+    os.chdir(build_path)
+    subprocess.check_call('mingw32-make -j4')
+    os.chdir(root_path)
+    if not os.path.isfile(root_path + "/Makefile"):
+        subprocess.check_call('cmake -G "MinGW Makefiles" ' + root_path, shell=True)
     subprocess.check_call('cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ' + os.getcwd(), shell=True)
 
 
